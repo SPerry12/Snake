ad6e5b5151359271c507febf12913ce7fd068a41
Shane Perry <perrysm98@gmail.com>
2022-05-08 18:36:28 -0500

    Set up classes and methods

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,11 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		// If you don't use the GameFrame you don't need to code out: GameFrame frame = new GameFrame();
+		new GameFrame();
+
+	}
+
+}
